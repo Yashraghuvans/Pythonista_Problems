ebff7adeb7aac1f2874ee91c994634a485016454
Yash Raghuvanshi <yashraghuvans@gmail.com>
2024-10-05 11:11:03 +0530

    Delete kth_missing_element.py

--- kth_missing_element.py
+++
@@ -1,8 +0,0 @@
-class Solution(object):
-    def findKthPositive(self, arr, k):
-        for i in range(0,len(arr)):
-            if(arr[i]<=k):
-                k=k+1
-            else:
-                break
-        return k
